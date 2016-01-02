85298e2a31d10ee653b82a0bad625cafd50761f9
Christophe Rosset <tophe@topheman.com>
2016-01-02 22:41:42 +0100

    feat(*) simple example with RxJS

--- src/containers/RxJSHome/RxJSHome.js
+++ src/containers/RxJSHome/RxJSHome.js
@@ -1,24 +1,43 @@
+/* eslint react/no-did-mount-set-state: 0 */
+
 import React from 'react';
+import Rx from 'rx';
 
 import { Link } from 'react-router';
 
-import { getSensorMode } from '../../services/sensorsObserver';
+import { getSensorMode, createMapEventFromMode, MODE_DEVICEORIENTATION } from '../../services/sensorsObserver';
 
-const RxJSHome = () => {
-  const sensorMode = getSensorMode();
-  console.log(sensorMode);
+import DisplayOneEvent from '../../components/RxJS/DisplayOneEvent/DisplayOneEvent';
 
-  return (
-    <div>
-      <ol className="breadcrumb">
-        <li><Link to="/" activeClassName="active">Home</Link></li>
-        <li className="active">RxJS</li>
-      </ol>
-      <p style={{marginTop: '10px'}}>Something about RxJS ...</p>
-      <p>This part (and others) will be working with both mouse and accelerometer, so to enjoy it, test it with your mobile/tablet AND your laptop/desktop.</p>
-      <p>Currently, <strong>{sensorMode.accelerometer ? 'an ' : 'no '}accelerometer has been detected on your device</strong>.</p>
-    </div>
-  );
-};
+export default class RxJSHome extends React.Component {
 
-export default RxJSHome;
+  constructor(props) {
+    super(props);
+    this.sensorMode = getSensorMode();
+    console.log(this.sensorMode);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    const uniformEvents = createMapEventFromMode(this.sensorMode);
+    this.observable = Rx.Observable.fromEvent(window, this.sensorMode).map(uniformEvents);
+    if (this.sensorMode === MODE_DEVICEORIENTATION) {
+      this.observable = this.observable.throttle(50);// only emit every 50ms
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ol className="breadcrumb">
+          <li><Link to="/" activeClassName="active">Home</Link></li>
+          <li className="active">RxJS</li>
+        </ol>
+        <p style={{marginTop: '10px'}}>Something about RxJS ...</p>
+        <p>This part (and others) will be working with both mouse and accelerometer, so to enjoy it, test it with your mobile/tablet AND your laptop/desktop.</p>
+        <p>Currently, <strong>{this.sensorMode === MODE_DEVICEORIENTATION ? 'an ' : 'no '}accelerometer has been detected on your device</strong>.</p>
+        <DisplayOneEvent sensorMode={this.sensorMode} observable={this.observable}/>
+      </div>
+    );
+  }
+}
--- src/services/sensorsObserver.js
+++ src/services/sensorsObserver.js
@@ -1,6 +1,11 @@
 import sensorsChecker from 'sensors-checker';
 
-const LOCALSTORAGE_PREFIX = '123456789';// to avoid namespace collisions
+const LOCALSTORAGE_PREFIX = '523456789';// to avoid namespace collisions
+
+export const MODE_DEVICEORIENTATION = 'deviceorientation';
+export const MODE_MOUSEMOVE = 'mousemove';
+
+/** ******** Simple localStorage abstraction **********/
 
 function storageGetItem(key) {
   return JSON.parse(localStorage.getItem(LOCALSTORAGE_PREFIX + key));
@@ -14,6 +19,12 @@ function storageIsSet(key) {
   return localStorage.getItem(LOCALSTORAGE_PREFIX + key) !== null;
 }
 
+/**
+ * This will check for accelerometer and return a promise when done, giving you the type of event to use:
+ * - deviceorientation (if accelerometer present)
+ * - mousemove (if no accelerometer)
+ * @returns {Promise|Promise<T>}
+ */
 export default function init() {
   return new Promise((resolve) => {
     // if check was already done on that device, no need to redo it
@@ -21,17 +32,46 @@ export default function init() {
       return resolve(storageGetItem('sensorMode'));
     }
     sensorsChecker.checkDeviceorientation(() => {
-      storageSetItem('sensorMode', {accelerometer: true});
-      return resolve({accelerometer: true});
+      const mode = MODE_DEVICEORIENTATION;
+      storageSetItem('sensorMode', mode);
+      return resolve(mode);
     }, () => {
-      storageSetItem('sensorMode', {accelerometer: false});
-      return resolve({accelerometer: false});
+      const mode = MODE_MOUSEMOVE;
+      storageSetItem('sensorMode', mode);
+      return resolve(mode);
     }, {
       userAgentCheck: /(iPad|iPhone|Nexus|Mobile|Tablet)/i
     });
   });
 }
 
+/**
+ * Returns the what you got in the rsolve of the promise
+ * @returns {String}
+ */
 export function getSensorMode() {
   return storageGetItem('sensorMode');
+}
+
+/**
+ * This function returns a function that will take an event and return an object with the same shape
+ * weather it's a mousemove or a deviceorientation (so that they could be handled the same way by the UI)
+ * @param mode
+ * @returns {Function}
+ */
+export function createMapEventFromMode(mode) {
+  switch (mode) {
+    case MODE_MOUSEMOVE:
+      return (e) => ({
+        a: e.clientX,
+        b: e.clientY
+      });
+    case MODE_DEVICEORIENTATION:
+      return (e) => ({
+        a: e.beta,
+        b: e.gamma
+      });
+    default:
+      throw new Error('Unhandled mode : ' + mode);
+  }
 }
