79246b4f388aad12351ff4c548b7df9f2dde32dd
Christophe Rosset <tophe@topheman.com>
2016-01-06 01:04:28 +0100

    feat(*) prepare census data client side

--- src/components/victory/PieChartPanel/PieChartPanel.js
+++ src/components/victory/PieChartPanel/PieChartPanel.js
@@ -31,7 +31,7 @@ export default class victoryPieChartPanel extends React.Component {
       <div className="panel panel-default pie-chart-panel">
         <div className="panel-heading">PieChart - World population 2010-2035</div>
         <div className="panel-body text-center">
-          <Slider min={2010} max={2035} step={1} originalValue={this.state.year} label="Year: %value" update={this.updateYear.bind(this)}/>
+          <Slider min={2010} max={2034} step={1} originalValue={this.state.year} label="Year: %value" update={this.updateYear.bind(this)}/>
           <Slider min={0} max={60} step={1} originalValue={this.state.innerRadius} label="InnerRadius: %valuepx" update={this.updateInnerRadius.bind(this)}/>
         </div>
       </div>
--- src/resources/helper.js
+++ src/resources/helper.js
@@ -1,14 +1,31 @@
-import fixtures from './fixtures/stackoverflow-survey.json';
+import stackoverflowFixtures from './fixtures/stackoverflow-survey.json';
+import censusFixtures from './fixtures/census-data-compressed.json';
+
+const censusPreparedData = censusFixtures.data.reduce((acc, curr) => {
+  // the key is the year
+  acc[curr[2]] = acc[curr[2]] ? acc[curr[2]] : {};
+  // the second key is the range of age
+  acc[curr[2]][curr[3]] = acc[curr[2]][curr[3]] ? acc[curr[2]][curr[3]] : {};
+  // for each range of age of each year, we take the rest of the infos
+  for (let i = 4; i <= 10; i++) {
+    acc[curr[2]][curr[3]][censusFixtures.headers[i]] = curr[i];
+  }
+  return acc;
+}, {});
+
+export const censusRawData = () => {
+  return censusPreparedData;
+};
 
 export const d3actPieExtractMostPopularTechnologiesByYear = (year) => {
-  return fixtures.mostPopularTechnologies[year].reduce((accumulator, current) => {
+  return stackoverflowFixtures.mostPopularTechnologies[year].reduce((accumulator, current) => {
     accumulator[current.name] = (current.score * 100).toPrecision(4);// 0.544 * 100 = 54.400000000000006 :(
     return accumulator;
   }, {});
 };
 
 export const d3actBarExtractMostPopularTechnologiesByYear = (year) => {
-  return fixtures.mostPopularTechnologies[year].reduce((accumulator, current) => {
+  return stackoverflowFixtures.mostPopularTechnologies[year].reduce((accumulator, current) => {
     accumulator.push({
       xValue: current.name,
       yValue: parseFloat((current.score * 100).toPrecision(4))// 0.544 * 100 = 54.400000000000006 :(
@@ -18,14 +35,14 @@ export const d3actBarExtractMostPopularTechnologiesByYear = (year) => {
 };
 
 export const d3actPieExtractDesktopOperatingSystemByYear = (year) => {
-  return fixtures.desktopOperatingSystem[year].reduce((accumulator, current) => {
+  return stackoverflowFixtures.desktopOperatingSystem[year].reduce((accumulator, current) => {
     accumulator[current.name] = (current.score * 100).toPrecision(4);// 0.544 * 100 = 54.400000000000006 :(
     return accumulator;
   }, {});
 };
 
 export const d3actBarExtractDesktopOperatingSystemByYear = (year) => {
-  return fixtures.desktopOperatingSystem[year].reduce((accumulator, current) => {
+  return stackoverflowFixtures.desktopOperatingSystem[year].reduce((accumulator, current) => {
     accumulator.push({
       xValue: current.name,
       yValue: parseFloat((current.score * 100).toPrecision(4))// 0.544 * 100 = 54.400000000000006 :(
