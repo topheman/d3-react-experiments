155ffb3a45a7a6405860ee08e17e9f9aeee58268
Christophe Rosset <tophe@topheman.com>
2016-01-09 13:52:37 +0100

    feat(*) add navigator decorator

--- src/containers/victory/WorldPopulationSexRatio.js
+++ src/containers/victory/WorldPopulationSexRatio.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import Navigator from '../../components/Navigator/Navigator';
+import navigator from '../../utils/react/navigator';
 import WorldPopulationSexRatio from '../../components/victory/WorldPopulationSexRatio/WorldPopulationSexRatio';
 
 const VictoryWorldPopulationSexRatio = () => (
   <div>
-    <Navigator prev="/d3act-mixed" next="/d3act-barchart">
     <h2>Victory</h2>
     <WorldPopulationSexRatio/>
     <p>Data comes from <a href="http://www.census.gov/population/international/data/idb/informationGateway.php" title="census.gov">census.gov</a></p>
-    </Navigator>
   </div>
 );
 
-export default VictoryWorldPopulationSexRatio;
+export default navigator()(VictoryWorldPopulationSexRatio);
---
+++ src/utils/react/navigator.js
@@ -0,0 +1,76 @@
+const defaultLinks = [
+  '/victory-world-population-sex-ratio',
+  '/d3act-barchart',
+  '/d3act-mixed'
+];
+
+import React from 'react';
+import { Link } from 'react-router';
+
+const getPrevNextLinks = (location, links = defaultLinks) => {
+  const currentPath = location.pathname;
+  const prevNextLinks = {};
+  // 0 or 1 -> dont display pager
+  if (links.length < 2) {
+    return prevNextLinks;
+  }
+  // only 2 -> always loop on next
+  if (links.length === 2) {
+    prevNextLinks.next = links.filter(link => link !== currentPath)[0];
+    return prevNextLinks;
+  }
+  // more links
+  if (links.indexOf(currentPath) === 0) {
+    prevNextLinks.prev = links[links.length - 1];
+    prevNextLinks.next = links[1];
+  }
+  else if (links.indexOf(currentPath) === links.length - 1) {
+    prevNextLinks.prev = links[links.length - 2];
+    prevNextLinks.next = links[0];
+  }
+  else {
+    const currentPathIndex = links.indexOf(currentPath);
+    prevNextLinks.prev = links[currentPathIndex - 1];
+    prevNextLinks.next = links[currentPathIndex + 1];
+  }
+  return prevNextLinks;
+};
+const getDisplayName = (WrappedComponent) => {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+};
+
+const navigator = (links) => WrappedComponent => {
+  const Navigator = ({props}, {location}) => {
+    const {prev, next} = getPrevNextLinks(location, links);
+    if (prev || next) {
+      return (
+        <div>
+          <nav>
+            <ul className="pager">
+              {prev ? <li className="previous"><Link to={prev}>Previous</Link></li> : null}
+              {next ? <li className="next"><Link to={next}>Next</Link></li> : null}
+            </ul>
+          </nav>
+          <WrappedComponent {...props}/>
+          <nav>
+            <ul className="pager">
+              {prev ? <li className="previous"><Link to={prev}>Previous</Link></li> : null}
+              {next ? <li className="next"><Link to={next}>Next</Link></li> : null}
+            </ul>
+          </nav>
+        </div>
+      );
+    }
+    return (<WrappedComponent {...props}/>);
+  };
+  Navigator.displayName = `Navigator(${getDisplayName(WrappedComponent)})`;
+  Navigator.contextTypes = {
+    location: React.PropTypes.object.isRequired
+  };
+  Navigator.propTypes = {
+    links: React.PropTypes.arrayOf(React.PropTypes.string)
+  };
+  return Navigator;
+};
+
+export default navigator;
