54d40a33acfc786e160f630b0f06db12ffe64153
Christophe Rosset <tophe@topheman.com>
2016-01-14 00:27:19 +0100

    correct hash

--- assets/js/bundle-devtools-5df833ba98e0812fe9c8.js
+++ assets/js/bundle-devtools-5df833ba98e0812fe9c8.js
@@ -4,7 +4,7 @@
  * Mix d3 & react
  * 
  * @version v0.0.1 - 14/01/2016
- * @revision #1c150c7 - https://github.com/topheman/d3-react-experiments/tree/1c150c7dec7c821361237469f8415dce66745c56
+ * @revision #0627f55 - https://github.com/topheman/d3-react-experiments/tree/0627f55bd6561c6a25b88b93dee529d4c67823f3
  * @author Christophe Rosset
  * @copyright 2016(c) Christophe Rosset
  * @license MIT
