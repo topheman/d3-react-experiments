b748614ad419d69cf7c668976a64eef5bd7c1468
Christophe Rosset <tophe@topheman.com>
2016-09-17 21:00:26 +0200

    refactor(*): renamed files to fit folder containers convention

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,5 @@
 export { default as App } from './App/App';
 export { default as AppHome } from './Home/Home';
-export { default as D3actBarChart } from './d3act/BarChart';
-export { default as D3actMixed } from './d3act/MixedChart';
-export { default as VictoryWorldPopulation } from './victory/WorldPopulationByAgeRange';
+export { default as D3actBarChart } from './D3actBarChart/D3actBarChart';
+export { default as D3actMixedChart } from './D3actMixedChart/D3actMixedChart';
+export { default as VictoryWorldPopulationByAgeRange } from './VictoryWorldPopulationByAgeRange/VictoryWorldPopulationByAgeRange';
--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,15 @@ import {
   App,
   AppHome,
   D3actBarChart,
-  D3actMixed,
-  VictoryWorldPopulation
+  D3actMixedChart,
+  VictoryWorldPopulationByAgeRange
 } from './containers/index';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={AppHome}/>
     <Route path="d3act-barchart" component={D3actBarChart}/>
-    <Route path="d3act-mixed" component={D3actMixed}/>
-    <Route path="victory-world-population-by-age-range" component={VictoryWorldPopulation}/>
+    <Route path="d3act-mixed" component={D3actMixedChart}/>
+    <Route path="victory-world-population-by-age-range" component={VictoryWorldPopulationByAgeRange}/>
   </Route>
 );
