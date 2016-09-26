be140eaf3bf2a777b90ae30482f8fcff164c0bcc
Christophe Rosset <tophe@topheman.com>
2016-09-27 00:34:53 +0200

    v2.1.0

---
+++ main-160859cc6240f666a53d.js
@@ -0,0 +1,13 @@
+/*!
+ * d3-react-experiments
+ * 
+ * Mix d3 & react
+ * 
+ * @version v2.1.0 - 27/09/2016
+ * @revision #8f7905f - https://github.com/topheman/d3-react-experiments/tree/8f7905fe489252cd4147926d9296f8faf7b7856a
+ * @author Christophe Rosset
+ * @copyright 2016(c) Christophe Rosset
+ * @license MIT
+ * 
+ */
+!function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};return t.m=r,t.c=e,t.p="",t(0)}([function(r,t){}]);
